b368762e7120284938ad33a46fb739fc5ff5cb47
nourawael <nourawael99@gmail.com>
2024-08-28 18:17:17 +0300

    Add project files.

---
+++ MVCDemo/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ MVCDemo/Program.cs
@@ -0,0 +1,62 @@
+namespace MVCDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            app.UseRouting();
+            app.UseStaticFiles();
+
+            #region 
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapGet("/Home", async context =>
+            //    {
+            //        await context.Response.WriteAsync("You are at Home Page");
+            //    });
+
+
+            //    endpoints.MapGet("/Products/{id?: int?}", async context =>
+            //    {
+            //        var idData = context.Request.RouteValues["id"];
+            //        if (idData is not null)
+            //        {
+            //            int id = Convert.ToInt32(idData);
+            //            await context.Response.WriteAsync($"You requested Product with id ->{id}");
+            //        }
+            //        else
+            //            await context.Response.WriteAsync("You are at Products Page");
+
+            //    });
+            //    endpoints.MapGet("/Books/{id}/{author:alpha:minlength(4):maxlength(6)}", async context =>
+            //    {
+            //        int id = Convert.ToInt32(context.Request.RouteValues["id"]);
+            //        string author = context.Request.RouteValues["author"].ToString();
+
+            //        await context.Response.WriteAsync($"You requested Product with id ->{id} and author-> {author}");
+            //    });
+            //});
+            //app.Run(async (HttpContent) => 
+            //{
+            //    await HttpContent.Response.WriteAsync("Your Requested Page Not found");
+
+            //});
+            #endregion
+
+
+            app.MapControllerRoute(
+                name:"default",
+                pattern: "/{Controller=Home}/{Action=Index}",
+                defaults: new {Controller="Home", Action="Index"}
+                );
+
+
+            app.Run();
+        }
+    }
+}
